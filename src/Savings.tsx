cd0754213bdd05c44765952dee66edc1674e87f1
James Jansson <jamesjansson@gmail.com>
2024-11-07 15:52:21 +1100

    Calculate Savings

--- src/Savings.tsx
+++ src/Savings.tsx
@@ -7,32 +7,89 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import MainNavbar from "./MainNavbar";
 import Footer from "./Footer";
+import dayjs from "dayjs";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import isLeapYear from "dayjs/plugin/isLeapYear";
+
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isLeapYear);
 
 function Savings() {
-  const [state, setState] = useState({
+  const [state, setState] = useState<{
+    deposit: number;
+    interestRate: number;
+    period: number;
+    total: number;
+    totalInterest: number;
+    graphData: { label: string; balance: number; interest: number }[];
+  }>({
     deposit: 100,
     interestRate: 4.5,
     period: 30,
     total: 0,
     totalInterest: 0,
+    graphData: [],
   });
 
   function setDeposit(event: ChangeEvent<HTMLInputElement>) {
     const value = Number(event.target.value);
-
     setState((prev) => ({ ...prev, deposit: value }));
+    calculateSavings();
   }
 
   function setInterest(event: ChangeEvent<HTMLInputElement>) {
     const value = Number(event.target.value);
-
     setState((prev) => ({ ...prev, interestRate: value }));
+    calculateSavings();
   }
 
   function setPeriod(event: ChangeEvent<HTMLInputElement>) {
     const value = Number(event.target.value);
-
     setState((prev) => ({ ...prev, period: value }));
+    calculateSavings();
+  }
+
+  function calculateSavings() {
+    let currentDay = dayjs();
+    const endDay = currentDay.add(state.period, "year");
+
+    let balance = 0;
+    let interest = 0;
+    const graphData: { label: string; balance: number; interest: number }[] =
+      [];
+
+    let accumulatedMonthlyInterest = 0;
+    let count = 0;
+    while (currentDay.isSameOrBefore(endDay) && count < 1000 * 365) {
+      if (currentDay.day() === 0) balance += state.deposit;
+      const interestRate = currentDay.isLeapYear()
+        ? state.interestRate / 100 / 366
+        : state.interestRate / 100 / 365;
+      accumulatedMonthlyInterest += balance * interestRate;
+      if (currentDay.date() === 1) {
+        balance += accumulatedMonthlyInterest;
+        interest += accumulatedMonthlyInterest;
+
+        graphData.push({
+          label: currentDay.format("MMM-YY"),
+          balance,
+          interest,
+        });
+
+        accumulatedMonthlyInterest = 0;
+      }
+
+      currentDay = currentDay.add(1, "day");
+      count++;
+    }
+    console.log({ state });
+
+    setState((prev) => ({
+      ...prev,
+      total: balance,
+      totalInterest: interest,
+      graphData,
+    }));
   }
 
   return (
@@ -48,7 +105,7 @@ function Savings() {
                   aria-label="Weekly deposit amount"
                   onChange={setDeposit}
                 />
-                <InputGroup.Text>.00 per week</InputGroup.Text>
+                <InputGroup.Text>per week</InputGroup.Text>
               </InputGroup>
 
               <InputGroup className="mb-3" size="lg">
