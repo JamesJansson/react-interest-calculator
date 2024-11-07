f56339c6b2eb4ccda6fafb85fad832e737cc1cf9
James Jansson <jamesjansson@gmail.com>
2024-11-07 23:38:02 +1100

    Add useEffect and flatten state variables

--- src/Savings.tsx
+++ src/Savings.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -22,7 +22,7 @@ type GraphData = {
 };
 
 function Savings() {
-  function emptyData(): GraphData {
+  function emptyGraphData(): GraphData {
     return {
       labels: [],
       datasets: [
@@ -38,63 +38,55 @@ function Savings() {
     };
   }
 
-  const [state, setState] = useState<{
-    deposit: number;
-    interestRate: number;
-    period: number;
-    total: number;
-    totalInterest: number;
-    graphData: GraphData;
-  }>({
-    deposit: 100,
-    interestRate: 4.5,
-    period: 30,
-    total: 0,
-    totalInterest: 0,
-    graphData: emptyData(),
-  });
+  const [deposit, setDeposit] = useState(100);
+  const [interestRate, setInterestRate] = useState(3.5);
+  const [period, setPeriod] = useState(25);
+  const [finalBalance, setFinalBalance] = useState(0);
+  const [totalInterest, setTotalInterest] = useState(25);
+  const [graphData, setGraphData] = useState(emptyGraphData());
 
-  function setDeposit(event: ChangeEvent<HTMLInputElement>) {
+  function depositEvent(event: ChangeEvent<HTMLInputElement>) {
     const value = Number(event.target.value);
-    setState((prev) => ({ ...prev, deposit: value }));
-    calculateSavings();
+    setDeposit(value);
   }
 
-  function setInterest(event: ChangeEvent<HTMLInputElement>) {
+  function interestRateEvent(event: ChangeEvent<HTMLInputElement>) {
     const value = Number(event.target.value);
-    setState((prev) => ({ ...prev, interestRate: value }));
-    calculateSavings();
+    setInterestRate(value);
   }
 
-  function setPeriod(event: ChangeEvent<HTMLInputElement>) {
+  function periodEvent(event: ChangeEvent<HTMLInputElement>) {
     const value = Number(event.target.value);
-    setState((prev) => ({ ...prev, period: value }));
-    calculateSavings();
+    setPeriod(value);
   }
+
+  useEffect(() => {
+    calculateSavings();
+  }, [deposit, interestRate, period]);
 
   function calculateSavings() {
     let currentDay = dayjs();
-    const endDay = currentDay.add(state.period, "year");
+    const endDay = currentDay.add(period, "year");
 
     let balance = 0;
     let interest = 0;
-    const graphData = emptyData();
+    const gData = emptyGraphData();
 
     let accumulatedMonthlyInterest = 0;
     let count = 0;
     while (currentDay.isSameOrBefore(endDay) && count < 100 * 365) {
-      if (currentDay.day() === 0) balance += state.deposit;
-      const interestRate = currentDay.isLeapYear()
-        ? state.interestRate / 100 / 366
-        : state.interestRate / 100 / 365;
-      accumulatedMonthlyInterest += balance * interestRate;
+      if (currentDay.day() === 0) balance += deposit;
+      const dailyInterestRate = currentDay.isLeapYear()
+        ? interestRate / 100 / 366
+        : interestRate / 100 / 365;
+      accumulatedMonthlyInterest += balance * dailyInterestRate;
       if (currentDay.date() === 1) {
         balance += accumulatedMonthlyInterest;
         interest += accumulatedMonthlyInterest;
 
-        graphData.labels.push(currentDay.format("MMM-YY"));
-        graphData.datasets[0].data.push(balance.toFixed(2));
-        graphData.datasets[1].data.push(interest.toFixed(2));
+        gData.labels.push(currentDay.format("MMM-YY"));
+        gData.datasets[0].data.push(balance.toFixed(2));
+        gData.datasets[1].data.push(interest.toFixed(2));
 
         accumulatedMonthlyInterest = 0;
       }
@@ -102,14 +94,10 @@ function Savings() {
       currentDay = currentDay.add(1, "day");
       count++;
     }
-    console.log({ state });
 
-    setState((prev) => ({
-      ...prev,
-      total: balance,
-      totalInterest: interest,
-      graphData,
-    }));
+    setFinalBalance(balance);
+    setTotalInterest(interest);
+    setGraphData(gData);
   }
 
   return (
@@ -124,7 +112,7 @@ function Savings() {
                 <InputGroup.Text>$</InputGroup.Text>
                 <Form.Control
                   aria-label="Weekly deposit amount"
-                  onChange={setDeposit}
+                  onChange={depositEvent}
                 />
                 <InputGroup.Text>per week</InputGroup.Text>
               </InputGroup>
@@ -132,7 +120,7 @@ function Savings() {
               <InputGroup className="mb-3" size="lg">
                 <Form.Control
                   aria-label="Interest rate"
-                  onChange={setInterest}
+                  onChange={interestRateEvent}
                 />
                 <InputGroup.Text>%</InputGroup.Text>
               </InputGroup>
@@ -140,13 +128,13 @@ function Savings() {
               <InputGroup className="mb-3" size="lg">
                 <Form.Control
                   aria-label="Savings period"
-                  onChange={setPeriod}
+                  onChange={periodEvent}
                 />
                 <InputGroup.Text>years</InputGroup.Text>
               </InputGroup>
             </Col>
             <Col sm="12" md="8" className="align-self-center">
-              <Line data={state.graphData}></Line>
+              <Line data={graphData}></Line>
             </Col>
           </Row>
         </Container>
